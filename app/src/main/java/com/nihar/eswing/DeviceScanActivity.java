cfde1adc8cc0cecc8e5f39d1871766f8b7042698
yintothayang <yintothayang@gmail.com>
2015-05-08 19:33:50 -0700

    started to add example code for finding ble deviced

---
+++ app/src/main/java/com/nihar/eswing/DeviceScanActivity.java
@@ -0,0 +1,41 @@
+package com.nihar.eswing;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DeviceScanActivity extends ListActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    /*
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+     }
+     */
+ }
---
+++ app/src/main/java/com/nihar/eswing/eSwingInterface.java
@@ -0,0 +1,43 @@
+package com.nihar.eswing;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+/**
+ * Created by yin on 5/8/15.
+ */
+public class eSwingInterface {
+    private Context context;
+    private Activity activity;
+
+
+    public eSwingInterface(Context context, Activity activity){
+        this.context = context;
+        this.activity = activity;
+    }
+
+    //Connect to eSwing BLE
+    public void connect(){
+
+    }
+
+    //Set Player
+    public void setPlayer(){
+
+    }
+
+    //Set Club
+    public void setClub(){
+
+    }
+
+    //Take Shot
+    public void takeShot(){
+
+    }
+
+
+}
