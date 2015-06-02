8cea44c72a5c7390850347a548e681fd6158c950
Nishita Kheur <nishitak@uw.edu>
2015-06-01 19:05:06 -0700

    changes

--- app/src/main/java/com/nihar/eswing/ClubAdd.java
+++ app/src/main/java/com/nihar/eswing/ClubAdd.java
@@ -4,14 +4,86 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class ClubAdd extends ActionBarActivity {
+    Spinner spnr;
+    Spinner spnr1;
 
+    String[] skillLevel = {
+            "Player1",
+            "Player2",
+            "Player3",
+            "Player4",
+
+    };
+    String[] skillLevel1 = {
+            "Player1",
+            "Player2",
+            "Player3",
+            "Player4",
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_add);
+
+        spnr = (Spinner)findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, skillLevel);
+
+        spnr.setAdapter(adapter);
+        spnr.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                               int arg2, long arg3) {
+
+                        int position = spnr.getSelectedItemPosition();
+                        //Toast.makeText(getApplicationContext(), "You have selected " + skillLevel[+position], Toast.LENGTH_LONG).show();
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> arg0) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                }
+        );
+        spnr1 = (Spinner)findViewById(R.id.spinner3);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, skillLevel1);
+
+        spnr.setAdapter(adapter);
+        spnr.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                               int arg2, long arg3) {
+
+                        int position = spnr.getSelectedItemPosition();
+                        //Toast.makeText(getApplicationContext(), "You have selected " + skillLevel[+position], Toast.LENGTH_LONG).show();
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> arg0) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                }
+        );
     }
 
 
